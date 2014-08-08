2fe9daa47460d27c8c58f2957fcbc62674699f7c
yajd <yasir.ali@jdsu.com>
2014-08-08 14:36:00 -0700

    Update bootstrap.js
    looking real good so far.

--- bootstrap.js
+++ bootstrap.js
@@ -795,8 +795,8 @@ function updateStackDOMJson_basedOnToolkit(dontUpdateStack, iDOMWindow) { //and 
 //				console.log('stackDOMJson is 0 length', stackDOMJson);
 //				console.log('profToolkit=',profToolkit);
 				stackDOMJson = [
-					{nodeToClone:'PUIsync', identifier:'[label="Create New Profile"]', label:'Create New Profile', class:'PanelUI-profilist create', hidden:null, id:null, oncommand:null, status:null, tooltiptext:null, signedin:null, defaultlabel:null, errorlabel:null,  addEventListener:['command',createUnnamedProfile,false], style:'-moz-appearance: none; padding: 10px 0px 10px 15px; height: 100%; flex-grow:1; background-color:red;border:0;display:flex;'},
-					{nodeToClone:'PUIsync', identifier:'[path="' + ini[profToolkit.selectedProfile.name].props.Path + '"]', label:profToolkit.selectedProfile.name, class:'PanelUI-profilist', hidden:null, id:null, oncommand:null, tooltiptext:null, signedin:null, defaultlabel:null, errorlabel:null, status:'active', addEventListener:['command', makeRename, false], style:'-moz-appearance: none; padding: 10px 0px 10px 15px; height: 100%; flex-grow:1; background-color:red;border:0;display:flex;', props:{profpath:ini[profToolkit.selectedProfile.name].props.Path}}
+					{nodeToClone:'PUIsync', identifier:'[label="Create New Profile"]', label:'Create New Profile', class:'PanelUI-profilist create', hidden:null, id:null, oncommand:null, status:null, tooltiptext:null, signedin:null, defaultlabel:null, errorlabel:null,  addEventListener:['command',createUnnamedProfile,false], style:'-moz-appearance: none; padding: 10px 0px 10px 15px; height: 100%; flex-grow:1; background-color:blue;border:0;display:flex;'},
+					{nodeToClone:'PUIsync', identifier:'[path="' + ini[profToolkit.selectedProfile.name].props.Path + '"]', label:profToolkit.selectedProfile.name, class:'PanelUI-profilist', hidden:null, id:null, oncommand:null, tooltiptext:null, signedin:null, defaultlabel:null, errorlabel:null, status:'active', addEventListener:['command', makeRename, false], style:'-moz-appearance: none; padding: 10px 0px 10px 15px; height: 100%; flex-grow:1; background-color:blue;border:0;display:flex;', props:{profpath:ini[profToolkit.selectedProfile.name].props.Path}}
 				];
 				var profNamesCurrentlyInMenu = [ini[profToolkit.selectedProfile.name].props.Path];
 				stackUpdated = true;
@@ -811,7 +811,7 @@ function updateStackDOMJson_basedOnToolkit(dontUpdateStack, iDOMWindow) { //and 
 //							console.log('m.props.profpath is not in pathsInIni = ', 'm.props.profpath=', m.props.profpath, 'pathsInIni=', profToolkit.pathsInIni, 'ini=', ini)
 							stackUpdated = true;
 							stackDOMJson.splice(i, 1); //this takes care of deletes
-							i--;
+							i--;	
 						} else {
 //							console.log('this stack value is in profToolkit', 'stack val = ', m.props.profpath, 'pathsInIni', profToolkit.pathsInIni);
 							profNamesCurrentlyInMenu.push(m.props.profpath);
@@ -861,7 +861,7 @@ function updateStackDOMJson_basedOnToolkit(dontUpdateStack, iDOMWindow) { //and 
 //					console.log('splicing p = ', ini[p], 'stackDOMjson=', stackDOMJson);
 					stackUpdated = true;
 					(function(pClosure) {
-						var objToSplice = {nodeToClone:'PUIsync', identifier:'[path="' + ini[pClosure].props.Path + '"]', label:p, class:'PanelUI-profilist', hidden:null, id:null, oncommand:null, tooltiptext:null, signedin:null, defaultlabel:null, errorlabel:null,  status:'inactive', addEventListener:['command', launchProfile, false], addEventListener2:['mousedown', makeRename, false], style:'-moz-appearance:none; padding:10px 0 10px 15px; margin-bottom:-1px; border-top:1px solid rgba(24,25,26,0.14); border-bottom:1px solid transparent; border-right:0 none rgb(0,0,0); border-left:0 none rgb(0,0,0);', props:{profpath:ini[pClosure].props.Path}};
+						var objToSplice = {nodeToClone:'PUIsync', identifier:'[path="' + ini[pClosure].props.Path + '"]', label:p, class:'PanelUI-profilist', hidden:null, id:null, oncommand:null, tooltiptext:null, signedin:null, defaultlabel:null, errorlabel:null,  status:'inactive', addEventListener:['command', launchProfile, false], addEventListener2:['mousedown', makeRename, false], style:'-moz-appearance: none; padding: 10px 0px 10px 15px; height: 100%; flex-grow:1; background-color:red;border:0;display:flex;', props:{profpath:ini[pClosure].props.Path}};
 						
 						if (pClosure == profToolkit.selectedProfile.name) {
 							//should never happend because stackDOMJson length was not 0 if in this else of the parent if IT WIL CONTNIUE on this: if (profIdsCurrentlyInMenu.indexOf(p.id) > -1) { continue }
@@ -1451,7 +1451,7 @@ var windowListener = {
 //			//console.log('PUIcs.style.width',PUIcs.style.width);
 			var profilistHBoxJSON =
 			['xul:vbox', {id:'profilist_box'},
-				['xul:stack', {key:'profilist_stack', style:'width:100%;'},
+				['xul:stack', {key:'profilist_stack', style:'width:100%; overflow:hidden; transition:height 250ms;'},
 					['xul:box', {style:'display:flex; border-top-width: 1px; border-top-style: solid; border-top-color: rgba(24, 25, 26, 0.14);', class:'PanelUI-profilist-box'}, ['xul:toolbarbutton', {'id':'profilistLoading', label:'Loading Profiles...', disabled:'true', class:'PanelUI-profilist profilistLoading', status:'active', style:'-moz-appearance:none; padding:10px 0 10px 15px; margin-bottom:-1px; border-top:1px solid rgba(24,25,26,0.14); border-bottom:1px solid transparent; border-right:0 none rgb(0,0,0); border-left:0 none rgb(0,0,0);', key:'profilistLoading'}]]
 				]
 			];
