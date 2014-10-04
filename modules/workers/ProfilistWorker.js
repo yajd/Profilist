932317774017c73a255295452948e6f72677c13e
yajd <yasir.ali@jdsu.com>
2014-10-03 22:48:37 -0700

    fixed nix stuff still utnested thought
    * corection on D.CloseFd declare
    * fixed a bnch D. in nix stuff
    * making nix stuff rely on ostypes
    * created returnFirstLibThatOpens
    * UNTESTED

--- modules/workers/ProfilistWorker.js
+++ modules/workers/ProfilistWorker.js
@@ -104,116 +104,60 @@ function queryProfileLocked(IsRelative, Path, rootPathDefault, retPid) {
 		case 'webos': // Palm Pre
 		case 'android': //profilist doesnt support android (i dont think android has profiles, im not sure) but i include here anyways as its linux
 			//start - try to open libc
-			if (!('libc' in lib)) {
-				var libsToTry = ['libc.so.6', 'libc.so.7', 'libc.so.61.0', 'libc.so'];
-				for (var i=0; i<libsToTry.length; i++) {
-					try {
-						ctypes.open(libsToTry);
-						break; //only gets here if succesfully opens
-					} catch(ex) {
-						if (ex.message == 'couldn\'t open library ' + libsToTry[i]) {
-							//its ok keep going
-							if (i == libsToTry.length - 1)  {
-								throw new Error('None of the libraries to try could be opened, OS is: "' + OS.Constants.Sys.Name + '"');
-							}
-						} else {
-							throw ex;
-						}
-					}
-				}
+			if (!lib.libc) {
+				lib.libc = returnFirstLibThatOpens(['libc.so.6', 'libc.so.7', 'libc.so.61.0', 'libc.so']);
 			}
 			//end - try to open libc
 			
-			if (!('F_GETLK' in D)) {
-				D.F_GETLK = 5;
-				D.F_RDLCK = 0;
-				D.F_WRLCK = 1;
-				D.F_UNLCK = 2;
-				
-				/* http://linux.die.net/man/2/fcntl
-				 * typedef struct flock {
-				 * ...
-				 * short l_type;     //Type of lock: F_RDLCK, F_WRLCK, F_UNLCK
-				 * short l_whence;   //How to interpret l_start: SEEK_SET, SEEK_CUR, SEEK_END
-				 * off_t l_start;    //Starting offset for lock
-				 * off_t l_len;      //Number of bytes to lock
-				 * pid_t l_pid;      //PID of process blocking our lock (F_GETLK only) 
-				 * ...
-				 * };
-				 */
-				//order matters:
-				// http://chat.stackexchange.com/transcript/message/17822233#17822233
-				// https://ask.mozilla.org/question/1134/order-of-strcuture-matters-test-case-flock-for-use-by-fcntl/
-				D.flock = new ctypes.StructType('flock', [
-					{'l_type': ctypes.unsigned_short},
-					{'l_whence': ctypes.unsigned_short},
-					{'l_start': ctypes.unsigned_long},
-					{'l_len': ctypes.unsigned_long},
-					{'l_pid': ctypes.int}
-				]);
-			}
-			
-			var fcntl = checkFcntl(lockPaths.unixFcntl, retPid);
-			if (fcntl === -1) {
-				var sym = checkSym(lockPaths.unixSym);
-				if (sym === -1) {
-					throw new Error('Could not verify if profile is in use via fcntl NOR sym');
-				} else {
-					return sym;
-				}
-			} else {
-				return fcntl;
-			}
+			return checkLockNsiProfileToolkitWay(lockPaths.unixFcntl, retPid, false);
 			
 			break;
 		case 'darwin':
-			if (!('libc' in lib)) {
-				libc = ctypes.open('libc.dylib');
+			if (!lib.libc) {
+				lib.libc = returnFirstLibThatOpens(['libc.dylib']);
 			}
 			
-			if (!('F_GETLK' in D)) {
-				D.F_GETLK = 7;
-				D.F_RDLCK = 1;
-				D.F_WRLCK = 3;
-				D.F_UNLCK = 2;
-				
-				/* https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man2/fcntl.2.html
-				 *       struct flock {
-				 *           off_t       l_start;    // starting offset
-				 *           off_t       l_len;      // len = 0 means until end of file
-				 *           pid_t       l_pid;      // lock owner
-				 *           short       l_type;     // lock type: read/write, etc.
-				 *           short       l_whence;   // type of l_start
-				 *       };
-				 */
-				//order matters:
-				// http://chat.stackexchange.com/transcript/message/17822233#17822233
-				// https://ask.mozilla.org/question/1134/order-of-strcuture-matters-test-case-flock-for-use-by-fcntl/
-				D.flock = new ctypes.StructType('flock', [
-					{'l_start': ctypes.unsigned_long},
-					{'l_len': ctypes.unsigned_long},
-					{'l_pid': ctypes.int},
-					{'l_type': ctypes.unsigned_short},
-					{'l_whence': ctypes.unsigned_short}
-				]);
-			}
-			
-			var fcntl = checkFcntl(lockPaths.unixFcntl, retPid);
-			if (fcntl === -1) {
-				var sym = checkSym(lockPaths.macSym);
-				if (sym === -1) {
-					throw new Error('Could not verify if profile is in use via fcntl NOR sym');
-				} else {
-					return sym == 1 ? true : false;
-				}
-			} else {
-				return fcntl == 1 ? true : false;
-			}
+			return checkLockNsiProfileToolkitWay(lockPaths.unixFcntl, retPid, true);
 			
 			break;
 		
 		default:
 			throw new Error('OS not recognized for queryProfileLocked, OS is: "' + OS.Constants.Sys.Name + '"');
+	}
+}
+
+function returnFirstLibThatOpens(libsToTry) {
+	for (var i=0; i<libsToTry.length; i++) {
+		try {
+			return ctypes.open(libsToTry[i]);
+		} catch(ex) {
+			if (ex.message == 'couldn\'t open library ' + libsToTry[i]) {
+				if (i == libsToTry.length - 1)  {
+					throw new Error('None of the libraries to try could be opened, OS is: "' + OS.Constants.Sys.Name + '"');
+				} //else { //its ok keep going }
+			} else {
+				throw ex;
+			}
+		}
+	}
+	throw new Error('returnFirstLibThatOpens: SHOULD NEVER GET HERE');
+}
+
+function checkLockNsiProfileToolkitWay(lockPath, retPid, isMac) {
+	var fcntl = checkFcntl(lockPaths.unixFcntl, retPid);
+	if (fcntl === -1) {
+		if (isMac) {
+			var sym = checkSym(lockPaths.macSym);
+		} else {
+			var sym = checkSym(lockPaths.unixSym);
+		}
+		if (sym === -1) {
+			throw new Error('Could not verify if profile is in use via fcntl NOR sym');
+		} else {
+			return sym; //return sym == 1 ? true : false;
+		}
+	} else {
+		return fcntl; //return fcntl == 1 ? true : false;
 	}
 }
 
@@ -224,7 +168,7 @@ function checkFnctl(lockPath, retPid) {
 	//1 = LOCKED
 	//-1 = fnctl failed (fnctl not available)
 	
-	if (!('fcntl' in D)) {
+	if (!D.fcntl) {
 		try {
 			//int fcntl(int fd, int cmd, ... /* arg */ );
 			/*
@@ -245,7 +189,7 @@ function checkFnctl(lockPath, retPid) {
 		}
 	}
 	
-	if (!('openFd' in D)) {
+	if (!D.openFd) {
 		//int open(const char *pathname, int flags, mode_t mode);
 		//https://github.com/downthemall/downthemall-mirror/blob/c8fd56c464b2af6b8dc7ddee1f9bbe6e9f6e8382/modules/manager/worker_posix.js#L35
 		D.openFd = lib.libc.declare(
@@ -257,8 +201,8 @@ function checkFnctl(lockPath, retPid) {
 		);
 	}
 	
-	if (!('closeFd' in D)) {
-		D.closeFd = _libc.declare(
+	if (!D.closeFd) {
+		D.closeFd = lib.libc.declare(
 			'close',
 			ctypes.default_abi,
 			ctypes.int, // retval
@@ -266,7 +210,7 @@ function checkFnctl(lockPath, retPid) {
 		);
 	}
 	
-	var fd = openFd(lockPath, OS.Constants.libc.O_RDWR | OS.Constants.libc.O_CREAT); //setting this to O_RDWR fixes errno of 9 on fcntl
+	var fd = D.openFd(lockPath, OS.Constants.libc.O_RDWR | OS.Constants.libc.O_CREAT); //setting this to O_RDWR fixes errno of 9 on fcntl
 	if (fd == -1) {
 		//if file does not exist and O_CREAT was not set. errno is == 2
 		//if file is a dangling symbolic link. errno is == 2
@@ -275,13 +219,13 @@ function checkFnctl(lockPath, retPid) {
 	}
 	
 	try {
-		var testlock = new flock();
-		testlock.l_type    = F_WRLCK; //can use F_RDLCK but keep openFd at O_RDWR, it just works
+		var testlock 	   = ostypes.flock();
+		testlock.l_type    = ostypes.F_WRLCK; //can use F_RDLCK but keep openFd at O_RDWR, it just works
 		testlock.l_start   = 0;
 		testlock.l_whence  = OS.Constants.libc.SEEK_SET;
 		testlock.l_len     = 0;
 		
-		var rez = fcntl(fd, F_GETLK, testlock.address());
+		var rez = fcntl(fd, ostypes.F_GETLK, testlock.address());
 		//console.log('rez:', rez);
 		if (rez != -1) {
 			//check testlock.l_type
@@ -292,14 +236,14 @@ function checkFnctl(lockPath, retPid) {
 				return parseInt(testlock.l_pid) == 0 ? 0 : 1;
 			}
 			/*
-			if (testlock.l_type == F_UNLCK) {
+			if (testlock.l_type == ostypes.F_UNLCK) {
 				//can also test if testlock.l_pid is not 0
 				//console.info('file is NOT locked');
 				return 0;
-			} else if (testlock.l_type == F_WRLCK) {
+			} else if (testlock.l_type == ostypes.F_WRLCK) {
 				//console.info('file is WRITE LOCKED, it may be read locked too');
 				return 1;
-			} else if (testlock.l_type == F_RDLCK) {
+			} else if (testlock.l_type == ostypes.F_RDLCK) {
 				//console.info('file is NOT write locked but just READ LOCKED'); //we know this because testlock tested for write lock first
 				return 1;
 			} else {
@@ -312,7 +256,7 @@ function checkFnctl(lockPath, retPid) {
 			retNeg1
 		}
 	} finally {
-		var rez = closeFd(fd);
+		var rez = D.closeFd(fd);
 		if (rez == 0) {
 			//console.log('succesfully closed, rez:', rez);
 		} else {
